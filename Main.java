487b823f43d7f830baa55ac60a10ee6a96fd7d1b
aditya91social <amanaditya@91social.com>
2023-10-14 17:57:10 +0530

    Adding new java file

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       String str1 = "Aditya";
+       String str2 = "Aditya";
+       String str3 = " Aman";
+       String str4 = "Aditya Aman";
+       String str5 = str2+str3;
+        System.out.println(str1==str2);
+        System.out.println(str1.equals(str2));
+        System.out.println(str4==str5);
+    }
+}
